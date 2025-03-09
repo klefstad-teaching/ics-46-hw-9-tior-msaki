a114b452171e2f3b1ce6c8785a2b0022bb851cf1
Rebecca Tio <tior@uci.edu>
2025-03-09 10:41:14 +0000

    load words done

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -52,6 +52,11 @@ bool is_adjacent_equal_len(const string& word1, const string& word2){
         }
 }
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
-void load_words(set<string> & word_list, const string& file_name);
+void load_words(set<string> & word_list, const string& file_name){
+    ifstream in(filename);
+    for (string word: in >> word;)
+        word_list.insert(word);
+    in.close();
+}
 void print_word_ladder(const vector<string>& ladder);
 void verify_word_ladder();
