7c0050282638e6400df46d0ba8ad1692defaa31a
Rebecca Tio <tior@uci.edu>
2025-03-10 13:12:07 +0000

    print_path done

---
+++ src/dijkstras.cpp
@@ -0,0 +1,8 @@
+#include "dijkstras.h"
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous);
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
+void print_path(const vector<int>& v, int total){
+    for (auto vertex : v) cout << vertex << ' ';
+    cout << "Total cost is " << total << endl;
+}
