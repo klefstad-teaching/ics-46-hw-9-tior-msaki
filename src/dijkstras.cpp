397f74d7e984a4a2bcc15876cdce97a88da8d82e
Rebecca Tio <tior@uci.edu>
2025-03-10 16:45:56 +0000

    more bug fixes

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,13 +6,35 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     distances[source] = 0;
     priority_queue<Vertex> pq;
 
-    for (auto edge : G[source]) {
-        if (distances[edge.dst]> edge.weight) {
-            distances[edge.dst] = edge.weight;
-            pq.push(Vertex(edge.dst, distances[edge.dst]));
+    Vertex current;
+    int vertex, curr_dist, new_dist;
+
+    while (!pq.empty()) {
+        current = pq.top();
+        pq.pop();
+        vertex = current.vertex;
+        curr_dist = current.weight;
+
+        if (curr_dist>distances[vertex]) continue;
+
+        for (auto edge: G[vertex]) {
+            new_dist = curr_dist + edge.weight;
+
+            if(new_dist<distances[edge.dst]) {
+                distances[edge.dst] = new_dist;
+                pq.push(Vertex(edge.dst, new_dist));
+                previous[edge.dst] = vertex;
             }
+        }
     }
-    previous[pq.top().vertex] = source;
+
+    // for (auto edge : G[source]) {
+    //     if (distances[edge.dst]> edge.weight) {
+    //         distances[edge.dst] = edge.weight;
+    //         pq.push(Vertex(edge.dst, distances[edge.dst]));
+    //         }
+    // }
+    // previous[pq.top().vertex] = source;
     // distances[source] = 0;
     return distances;
 
@@ -55,7 +77,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 }
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination){
     vector<int> short_path;
-    if (distances.size() == 0) return short_path;
+    if (distances.size() == 0 || previous.size() == 0) return short_path;
     short_path.push_back(destination);
     while (short_path.size()<previous.size()) {
         short_path.push_back(previous[short_path.back()]);
